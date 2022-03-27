4a72851233c81acaf96495b01f188e6c4a12f771
Edode <git-lisandro@x394.org>
2022-03-27 16:23:58 -0400

    Heavily modified Struct and file structure

---
+++ bot/message/orders.go
@@ -0,0 +1,49 @@
+package message
+
+import (
+	"bot/morse"
+	"bytes"
+	"encoding/base32"
+	"github.com/vmihailenco/msgpack"
+)
+
+type Bot struct {
+	Uid  [16]byte
+	Version [8]byte
+	Com    Commands
+}
+
+type Commands struct {
+	Command  []byte
+	Data []byte
+}
+
+func (b *Bot) unmarshal() []byte {
+	var buf bytes.Buffer
+	enc := msgpack.NewEncoder(&buf)
+	enc.Encode(b)
+	return buf.Bytes()
+}
+
+func (b *Bot) Deobfuscate_data() []byte {
+	var buf bytes.Buffer
+	enc := msgpack.NewEncoder(&buf)
+	enc.Encode(b)
+	return buf.Bytes()
+}
+
+func (b *Bot) marshal() []byte {
+	var buf bytes.Buffer
+	enc := msgpack.NewEncoder(&buf).StructAsArray(true)
+	err := enc.Encode(&b)
+	if err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
+func (b *Bot) Obfuscate_data() []byte {
+	var encrypted_data = Encrypt_data(b.marshal())
+	var encoded_data = base32.StdEncoding.EncodeToString(encrypted_data)
+	return morse.Encode(encoded_data)
+}
